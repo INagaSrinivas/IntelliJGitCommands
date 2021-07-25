8da07b545dde57c5ddf422d4ae5c15982144d45c
INagaSrinivas <srinu.sribala@gmail.com>
2021-07-25 22:07:16 +0530

    Added welcome message

--- src/SampleClass.java
+++ src/SampleClass.java
@@ -3,6 +3,7 @@ public class SampleClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hi Hello world");
+        System.out.println("Welcome Hello world");
     }
 
 
